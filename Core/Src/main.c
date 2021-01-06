db277f29ea0e92d9bb190c3960c743fc2fbb7085
Volkalex28 <anan20000228@gmail.com>
2021-01-06 18:45:45 +0200

    blink

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,6 +1,21 @@
-#include "stdint.h"
+#include "main.h"
+
+#define DELAY   100000
 
 int main() {
 
-    return 0 ;
+    RCC->CR |= RCC_CR_HSION;
+
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    // --- GPIO setup ----
+    GPIOC->MODER |= GPIO_MODER_MODER9_0;
+    //GPIOC->OTYPER &= ~(uint32_t)GPIO_OTYPER_OT_9
+    //GPIOC->OSPEEDR &= ~(uint32_t)GPIO_OSPEEDR_
+
+    while(1) {
+        GPIOC->BRR = GPIO_PIN_9;
+        for(int i = 0; i < DELAY; i++) {};
+        GPIOC->BSRR = GPIO_PIN_9;
+        for(int i = 0; i < DELAY; i++) {};
+    }
 }
